df824eb6d3da0996e26fd51764606da0929f0df5
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2024-04-24 17:35:45 -0400

    paquete guiaEjercicio2.ejercicio1 guiaEjercicio2.ejercicio2

--- src/guiaEjercicio2/ejercicio2/Main.java
+++ src/guiaEjercicio2/ejercicio2/Main.java
@@ -4,64 +4,92 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        //Se declaran las variables a utilizar
-        //El número que ingrese el usuario debe ser float
         float numero = 0f;
         float resultado = 0;
-        //Variable tipo String, ya que puede ser un operador o un número
         String ingreso = null;
-        //Variable que guardará el código ascii de cada ingreso del usuario
         int ascii = 0;
-        /* Se crea variable contador, para identificar si es el primer ingreso
-         * de usuario,el cual debe ser un número, y luego un operador, número
-         * así sucesivamente. Si contador es cero es inicio y debe ser un
-         * número. Si es impar, debe ser un operador +, -, = */
         int contador = 0;
         Scanner sc = new Scanner(System.in); //Se crea objeto Scanner
+
         do {
+
         if ((contador % 2) == 0) {
             System.out.println("Ingrese un número: ");
         } else {
-            System.out.println("Ingrese un operador (+, -, =): ");
+            System.out.println("Ingrese un operador (+, -, *, /, =): ");
             contador++;
         }
         //Se lee por pantalla el ingreso del usuario, operador o número
         ingreso = sc.next();
         // Códigos ascii para operadores * + --> 43, * - --> 45, * = --> 61 */
         ascii = (int) ingreso.charAt(0);
+        System.out.println(ascii);
         /* Si el programa inicio, es decir el contador está en cero y el usuario
          * no ingreso un operador (+, -, =) */
-        if (contador == 0 && ascii != 43 && ascii != 45 && ascii != 61) {
-            /* Se hace un cast de ingreso (String) a float. Todas las clases tienen un parse,
-            Integer.parseInteger, * Double.parseDouble * etc. Se hace de esta manera, porque un String no se
-            puede transformar en un número, sin embargo si el String no es un número, el programa dará un
-            error. */
+        if (contador == 0 && ascii != 43 && ascii != 45 && ascii != 42 && ascii != 47 && ascii != 61 ) {
             numero = Float.parseFloat(ingreso); //Se asigna a resultado el primer numero ingresado
+            System.out.println(numero);
             resultado = numero;
             contador++;
             // continúa en la página siguiente
         } else { // De lo contrario, es el segundo ingreso de usuario
             //Suma
             if (ascii == 43) {
-                System.out.println("Ingrese un número: ");
-                ingreso = sc.next(); //Debiese ser un número o si no error
-                numero = Float.parseFloat(ingreso);
-                resultado = resultado + numero;
+                resultado = suma(ingreso,numero,resultado);
                 contador++;
             }
             //Resta
             if (ascii == 45) {
-                System.out.println("Ingrese un número: ");
-                ingreso = sc.next(); //Debiese ser un número o si no error
-                numero = Float.parseFloat(ingreso);
-                resultado = resultado - numero;
+                resultado = resta(ingreso,numero,resultado);
+                contador++;
+            }
+            //multiplicar
+            if(ascii == 42){
+                System.out.println("bandera 1");
+                resultado = multiplicar(ingreso,numero,resultado);
+                contador++;
+            }
+            //dividir
+            if(ascii == 47){
+                resultado = dividir(ingreso,numero,resultado);
                 contador++;
             }
         }
-    }while((int)ingreso.charAt(0) != 61);
+        }while((int)ingreso.charAt(0) != 61);
 
-    System.out.printf("El Resultado es: %f", resultado);
+        System.out.printf("El Resultado es: %f", resultado);
+    }
 
+    public static float suma(String ingreso, float numero, float resultado){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        ingreso = sc.next(); //Debiese ser un número o si no error
+        numero = Float.parseFloat(ingreso);
+        return resultado = resultado + numero;
+    }
+
+    public static float resta(String ingreso, float numero, float resultado){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        ingreso = sc.next(); //Debiese ser un número o si no error
+        numero = Float.parseFloat(ingreso);
+        return resultado = resultado - numero;
+    }
+
+    public static float multiplicar(String ingreso, float numero, float resultado){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        ingreso = sc.next(); //Debiese ser un número o si no error
+        numero = Float.parseFloat(ingreso);
+        return resultado = resultado * numero;
+    }
+
+    public static float dividir(String ingreso, float numero, float resultado){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        ingreso = sc.next(); //Debiese ser un número o si no error
+        numero = Float.parseFloat(ingreso);
+        return resultado = resultado / numero;
     }
 
 }
