2bd4fec709b027d828cabbcdb9637990abf36400
Miguel Rondanelli Nuñez <90483570+Izth4rDev@users.noreply.github.com>
2024-06-08 19:37:04 -0400

    Miguel (#22)
    * Ejercicios 1 2 y 3 de la guia de ejercicios
    
    * Añadido paquete y clases de ejercicioOpcional3 y ejercicioGuido1
    
    * modificando Readme del proyecto
    
    * v2 readme
    
    * readme v3
    
    * v4 Readme
    
    * readme v5
    
    * añadido guia de ejercicio2 ejercicio1 y ejercicio2
    
    * paquete guiaEjercicio2.ejercicio1 guiaEjercicio2.ejercicio2
    
    * Readme v5
    
    * nada nuevo
    
    * Estructura de archivos del repo actualizada
    
    * README v6
    
    * añadido clase multiplos de 3
    
    * añadido ejercicio MultiplosDe3
    
    * README v7
    
    * README v8
    
    * Readme v7 mas Promedios.java
    
    * README v8
    
    * README v9
    
    * README v10, resolucion conflicots rama Miguel
    
    * modificado el Desafio4, añadido algunos elementos en el metodo crearArchivo
    
    * Añadido directorio del Desafio5 y actualizado el README
    
    * Desafio evaluado dia 28

--- src/cl/praxis/Desafio7/Menu.java
+++ src/cl/praxis/Desafio7/Menu.java
@@ -1,5 +1,50 @@
 package cl.praxis.Desafio7;
 
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+    private ArchivoServicio archService = new ArchivoServicio();
+    private ProductoServicio ps = new ProductoServicio();
+    private Scanner sc = new Scanner(System.in);
+    private Utilidad utility = new Utilidad();
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        menu.llamarMenu();
+    }
+
+    public void llamarMenu(){
+        int option ;
+        do {
+            utility.limpiarPantalla();
+            System.out.println("1 listar productos");
+            System.out.println("2 Editar datos");
+            System.out.println("3 Importar datos");
+            System.out.println("4 Salir del menu");
+            System.out.println("Ingrese una opcion: ");
+            option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option){
+                case 1: ps.listarProductos();
+                        utility.pausa(2);
+                break;
+                case 2:
+                    if(ps.getListaProductos().isEmpty()){
+                        System.out.println(" ERROR!!! tiene que cargar los datos primero desde el archivo - seleccione opcion 3");
+                        utility.pausa(2);
+                    }else{
+                        ps.editarProducto(archService);
+                        utility.pausa(2);
+                    }
+                    break;
+                case 3: archService.cargarDatos(ps);
+                         utility.pausa(2);
+                break;
+                case 4: System.out.println("Saliendo del programa");
+                        utility.pausa(2);
+                break;
+                default: System.out.println("Error ingrese una opcion valida");
+            }
+        }while(option!=4);
+    }
 }
