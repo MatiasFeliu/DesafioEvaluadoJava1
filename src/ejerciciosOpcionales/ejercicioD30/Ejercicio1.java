71e2c331d46eb3f1477b458c63a54f46009ce99f
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2024-06-10 19:24:37 -0400

    Correccion archivo csb

--- src/ejerciciosOpcionales/ejercicioD30/Ejercicio1.java
+++ src/ejerciciosOpcionales/ejercicioD30/Ejercicio1.java
@@ -1,5 +1,16 @@
 package ejerciciosOpcionales.ejercicioD30;
 
-public class Ejercicio1 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ejercicio1 {
+    private List<String> marcas;
+
+    public Ejercicio1(){
+        this.marcas = new ArrayList<>();
+    }
+
+    public void agregarMarca(String marca){
+        marcas.add(marca);
+    }
 }
